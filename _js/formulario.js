b1db867b60c144d6ae76bf061ef3019776e0237d
EduardaCarvalho-dv <eduardasantos132018@gmail.com>
2024-01-28 19:19:13 -0300

    forms

--- _js/formulario.js
+++ _js/formulario.js
@@ -1,4 +1,4 @@
-function adicionaLinha(nome, nota) {
+/*function adicionaLinha(nome, nota) {
     var conteudo = document.getElementById("conteudo");
 
     var novaLinha = conteudo.insertRow();
@@ -8,4 +8,49 @@ function adicionaLinha(nome, nota) {
 
     celulaNome.innerText = nome;
     celulaNota.innerText = nota;
+};*/
+
+function adicionar() {
+    var nomeA = document.getElementById('nome').value;
+    var notaA = document.getElementById('nota').value;
+    var content = document.getElementById('conteudo');
+
+
+    var aluno = { nome: nomeA, nota: notaA };
+    var guarda = JSON.parse(localStorage.getItem('alunos')) || [];
+
+    guarda.push(aluno);
+    localStorage.setItem('alunos', JSON.stringify(guarda));
+
+    var qtdlinhas = content.rows.length;
+    var novaLinha = content.insertRow(qtdlinhas);
+
+    var celulaNome = novaLinha.insertCell(0);
+    var celulaNota = novaLinha.insertCell(1);
+
+    celulaNome.innerHTML = nomeA;
+    celulaNota.innerHTML = notaA;
+
+    document.getElementById('formulario').reset();
+    return;
 };
+
+
+function carregarLocalStorage() {
+    var guarda = JSON.parse(localStorage.getItem('alunos')) || [];
+    var content = document.getElementById('conteudo');
+
+    guarda.forEach(function (aluno) {
+        var qtdlinhas = content.rows.length;
+        var novaLinha = content.insertRow(qtdlinhas);
+
+        var celulaNome = novaLinha.insertCell(0);
+        var celulaNota = novaLinha.insertCell(1);
+
+        celulaNome.innerHTML = aluno.nome;
+        celulaNota.innerHTML = aluno.nota;
+
+    });
+}
+
+document.addEventListener('DOMContentLoaded', carregarLocalStorage);
